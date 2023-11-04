4d6795d0040ea4654020d55e88f3822cda1113f0
Jason Francis <121530054+BL-MSCH-C220@users.noreply.github.com>
2023-11-03 21:33:00 -0400

    Getting started
    I started connecting a few things. Could you take a look and let me know what I could (specifically) do to help? I just want to make sure I understand your goals.

--- Levels/Camera.gd
+++ Levels/Camera.gd
@@ -9,8 +9,8 @@ func _ready():
 func _physics_process(_delta):
 	player = get_node_or_null("/root/Game/Player_Container/Player")
 	if player != null:
-		zoom = Vector2(2.0,2.0)
+		zoom = Vector2(1.0,1.0)
 		global_position = player.global_position
 	else:
-		zoom = Vector2(1,1)
+		zoom = Vector2(0.5,0.5)
 		position = Vector2(0,0)
